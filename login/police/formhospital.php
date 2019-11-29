c5a7288cf117f4ec38ab9688da05a50958e74f95
Ashan Tharindu <ashh.tt99@gmail.com>
2019-11-29 20:00:13 +0530

    Adding New Fields

--- login/police/formhospital.php
+++ login/police/formhospital.php
@@ -21,13 +21,20 @@ $district = $_POST['district'];
 $other = $_POST['other'];
 $other2 = $_POST['other2'];
 $dna = $_POST['dna'];
+$fingerprint = $_POST['fingerprint'];
+$dental = $_POST['dental'];
+$facialphotograph = $_POST['facialphotograph'];
+$clothes = $_POST['clothes'];
+$ornaments = $_POST['ornaments'];
+$tattoos = $_POST['tattoos'];
+$specialremarks = $_POST['specialremarks'];
 
 
-$sql="INSERT INTO unidentifiedbodies (srno,date,policearea,province,district,other,other2,dna,)
+$sql="INSERT INTO unidentifiedbodies (srno,date,policearea,province,district,other,other2,dna,fingerprint,dental,facialphotograph,clothes,ornaments,tattoos,specialremarks)
 
 VALUES
 
-('$srno','$date','$policearea','$province','$district','$other','$other2','$dna',)";
+('$srno','$date','$policearea','$province','$district','$other','$other2','$dna','$fingerprint','$dental','$facialphotograph','$clothes','$ornaments','$tattoos','$specialremarks')";
 
  
 
