50e5225b36148dbb64f98eb36928e4d8c36ce834
Ashan Tharindu <ashh.tt99@gmail.com>
2019-12-15 20:19:44 +0530

    Adding database connection

--- login/admin/view.php
+++ login/admin/view.php
@@ -36,7 +36,51 @@
     </nav>
 
     <h2 style="color:black; text-align: center; padding-top: 10%;">Registered Users</h2>
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "faceid");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt select query execution
+$sql = "SELECT * FROM users";
 
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table 
+                border=1 
+                align=center 
+                class=table table-dark table-striped
+
+            >";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>User_Name</th>";
+                echo "<th>Edit</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['username'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
 
     
     <script src="assets/js/jquery.min.js"></script>
