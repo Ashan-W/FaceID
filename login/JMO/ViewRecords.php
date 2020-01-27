949d2e77ebacf6aa146b6455dc3c281e1e353a3c
MadhaviBandara <madi.bandara@gmail.com>
2020-01-27 10:30:03 +0530

    View records

--- login/JMO/ViewRecords.php
+++ login/JMO/ViewRecords.php
@@ -15,17 +15,24 @@
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
         <div class="container"><a class="navbar-brand" href="#page-top">FaceID</a><button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarResponsive" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div class="collapse navbar-collapse" style="position: fixed; margin-left: 600px;" id="navbarResponsive">
+            <div class="collapse navbar-collapse" style="position: fixed; margin-left: 450px;" id="navbarResponsive">
                 <ul class="nav navbar-nav ml-auto text-uppercase">
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="ViewRecords.php">View Records</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="AddDetails.php">Add Details</a></li>
-                   
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="ModelRequests.php">3D Model Requests</a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <?php $results = mysqli_query($link, "SELECT * FROM unidentifiedbodies where jmo='$username") ; ?>
+    <?php $results = mysqli_query($link, "SELECT * FROM unidentifiedbodies ") ; 
+    if (!$results) {
+    printf("Error: %s\n", mysqli_error($link));
+    exit();
+}
+    ?>
+
+
     <h2 style="color:black; text-align: center; padding-top: 10%;">Unidentified bodies</h2>
     <div class="table1" >
 
@@ -37,37 +44,23 @@
                 <th style="padding:10px; text-align:center">Province</th>
                 <th style="padding:10px; text-align:center">District</th>
                 <th style="padding:10px; text-align:center">Police Area</th>
-                <th style="padding:10px; text-align:center">DNA</th>
-                <th style="padding:10px; text-align:center">Fingerprint</th>
-                <th style="padding:10px; text-align:center">Dental</th>
-                <th style="padding:10px; text-align:center">Facial Photograpgh</th>
-                <th style="padding:10px; text-align:center">Clothes</th>
-                <th style="padding:10px; text-align:center">Ornaments</th>
-                <th style="padding:10px; text-align:center">Tattoos</th>
-                <th style="padding:10px; text-align:center">Special Remarks</th>
                 <th style="padding:10px; text-align:center"></th>
                 
             </tr>
         </thead>
 
         <?php while ($row = mysqli_fetch_array($results)) { ?>
+            <form method="get" action="ViewMoreRecords.php">
             <tr>
                 <td style="padding:10px; text-align:center"><?php echo $row['srno'];?></td>
                 <td style="padding:10px; text-align:center"><?php echo $row['date'];?></td>
                 <td style="padding:10px; text-align:center"><?php echo $row['province'];?></td>
                 <td style="padding:10px; text-align:center"><?php echo $row['district'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['policearea'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['dna'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['fingerprint'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['dental'];?></td>   
-                <td style="padding:10px; text-align:center"><?php echo $row['facialphotograph'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['clothes'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['ornaments'];?></td>
-                <td style="padding:10px; text-align:center"><?php echo $row['tattoos'];?></td> 
-                <td style="padding:10px; text-align:center"><?php echo $row['specialremarks'];?></td> 
-                <td style="padding:10px; text-align:center"><button type="button" class="btn btn-secondary">Edit</button></td>
+                <td style="padding:10px; text-align:center"><?php echo $row['policearea'];?></td> 
+                <input type="hidden" name="srno" id="srno" value="<?php echo $row['srno']; ?>" />
+                <td style="padding:10px; text-align:center"><button type="submit" class="btn btn-secondary">View</button></td>
             </tr>
-
+</form>
         <?php } ?>
     
     
